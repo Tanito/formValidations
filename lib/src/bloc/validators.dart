8b6fb414ce1e692a242661198b850346142d08af
Tanito <ulises.d83@gmail.com>
2021-05-17 16:45:42 -0400

    flutter form validations: mail & pass

---
+++ lib/src/bloc/validators.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+class Validators {
+  final validarEmail =
+      StreamTransformer<String, String>.fromHandlers(handleData: (email, sink) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regExp = new RegExp(pattern);
+
+    if (!regExp.hasMatch(email)) {
+      sink.addError('Email no es correcto');
+    }
+  });
+
+  final validarPassword = StreamTransformer<String, String>.fromHandlers(
+      handleData: (password, sink) {
+    if (password.length < 8) {
+      sink.addError('Más de 8 caracteres por favor');
+    }
+  });
+}
